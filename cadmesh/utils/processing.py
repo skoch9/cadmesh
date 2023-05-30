6c2e3a0b4f4e9d13193b64f18c45ea13bf2720f9
Chandu <20564967+itsmeeChandU@users.noreply.github.com>
2023-05-30 12:05:20 -0700

    working model before modifications

--- cadmesh/utils/processing.py
+++ cadmesh/utils/processing.py
@@ -9,11 +9,15 @@ import multiprocessing
 import functools
 from joblib import Parallel, delayed
 
+
 from ..core.step_processor import StepProcessor
+
+
 
 @contextlib.contextmanager
 def tqdm_joblib(tqdm_object):
     """Context manager to patch joblib to report into tqdm progress bar given as argument"""
+
     class TqdmBatchCompletionCallback(joblib.parallel.BatchCompletionCallBack):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
@@ -29,9 +33,7 @@ def tqdm_joblib(tqdm_object):
     finally:
         joblib.parallel.BatchCompletionCallBack = old_batch_callback
         tqdm_object.close()
-        
 
-    
 
 def with_timeout(timeout):
     def decorator(decorated):
@@ -40,36 +42,57 @@ def with_timeout(timeout):
             pool = multiprocessing.pool.ThreadPool(1)
             async_result = pool.apply_async(decorated, args, kwargs)
             try:
-                return async_result.get(timeout)
+                async_result.get(timeout)
             except multiprocessing.TimeoutError:
-                return False
+                logging.info(f'Task timed out for args: {args}, kwargs: {kwargs}')  # log the time out
+                return
+
         return inner
+
     return decorator
 
 
 @with_timeout(120.0)
 def process_single_step(sf, output_dir, log_dir, produce_meshes=True):
-    if produce_meshes:
-        sp = StepProcessor(sf, Path(output_dir), Path(log_dir))
-    else: 
-        sp = StepProcessor(sf, Path(output_dir), Path(log_dir), mesh_builder=None)
-    sp.load_step_file()
-    sp.process_parts()
-    return True        
-        
+    try:
+        if produce_meshes:
+            sp = StepProcessor(sf, Path(output_dir), Path(log_dir))
+        else:
+            sp = StepProcessor(sf, Path(output_dir), Path(log_dir), mesh_builder=None)
+        sp.load_step_file()
+        sp.process_parts()
+        return True
+    except multiprocessing.TimeoutError:
+        print("Timeout error")
+        return False
+    except Exception as e:
+        print("Exception error")
+        print(e)
+        return False
 
-def process_step_folder(data_path="data/simple", output_path="data/results_simple", log_path="data/log_simple", file_pattern="*/*.step", file_range=[0, -1]):
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+def process_step_folder(data_path="data/simple", output_path="data/results_simple", log_path="data/log_simple",
+                        file_pattern="*/*.step", file_range=[0, -1], chunk_size=10):
     data_dir = Path(data_path)
     output_dir = Path(output_path)
     log_dir = Path(log_path)
-    
+
     step_files = sorted(data_dir.glob(file_pattern))
     if file_range[1] == -1:
         step_files = step_files[file_range[0]:]
     else:
         step_files = step_files[file_range[0]:file_range[1]]
-            
-    with tqdm_joblib(tqdm(desc="Processing step files", total=len(step_files))) as progress_bar:
-        results = Parallel(n_jobs=4)(delayed(process_single_step)(sf, output_dir, log_dir) for sf in step_files)
-        
-    return results
+
+    step_file_chunks = list(chunks(step_files, chunk_size))
+
+    for i, step_file_chunk in enumerate(step_file_chunks):
+        # logging.info(f'Starting chunk {i + 1}/{len(step_file_chunks)}')
+        with tqdm_joblib(tqdm(desc=f"Processing step files chunk {i + 1}/{len(step_file_chunks)}",
+                              total=len(step_file_chunk))) as progress_bar:
+            Parallel(n_jobs=4)(delayed(process_single_step)(sf, output_dir, log_dir) for sf in step_file_chunk)
