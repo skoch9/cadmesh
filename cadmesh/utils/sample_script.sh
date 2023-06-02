97dc05afa444f3c14c2b2677e5d91d7f0197c8af
Chandu <20564967+itsmeeChandU@users.noreply.github.com>
2023-06-02 02:18:08 -0700

    fixed error for sample script

--- cadmesh/utils/sample_script.sh
+++ cadmesh/utils/sample_script.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 #SBATCH --time=00:30:00
-#SBATCH --account=def-teseo
+#SBATCH --account=def-someuser
 #SBATCH --job-name=step-process
 #SBATCH --mem-per-cpu=4G
-#SBATCH --cpus-per-task=1
-#SBATCH --array=0-4
+#SBATCH --cpus-per-task=4
+#SBATCH --array=0-4%5
 
-module load python/3.9.0
+module load python/3.7.0
 module load mpi4py/3.0.3
-
-# Activate Python environment
-source projects/def-teseo/madduri/envs/cad/bin/activate
 
 # Get the username for the path
 USER_NAME=$USER
 
 # Define paths relative to the scratch directory
-DATA_PATH="/home/$USER_NAME/scratch/madduri/Fusion360/segmentation/step/s2.0.1_extended_step/breps/step"
-OUTPUT_PATH="/home/$USER_NAME/scratch/madduri/Fusion360/segmentation/yaml/s2.0.1_extended"
-LOG_PATH="/home/$USER_NAME/scratch/madduri/Fusion360/segmentation/yaml/logs"
+DATA_PATH="/scratch/$USER_NAME/Fusion360/segmentation/step/s2.0.1_extended_step/breps/step"
+OUTPUT_PATH="/scratch/$USER_NAME/Fusion360/segmentation/yaml/s2.0.1_extended"
+LOG_PATH="/scratch/$USER_NAME/Fusion360/segmentation/yaml/logs"
 
 # Get the batch of files this task should process
-FILES=($(ls -1 "$DATA_PATH"/*.stp | sed -n "$((SLURM_ARRAY_TASK_ID*100+1)),$((SLURM_ARRAY_TASK_ID*100+100))p"))
+FILES=($(ls -1 "$DATA_PATH" | sed -n "$((SLURM_ARRAY_TASK_ID*100+1)),$((SLURM_ARRAY_TASK_ID*100+100))p"))
+
+# Activate your Python environment
+source projects/def-teseo/$USER_NAME/envs/cad/bin/activate
 
 for FILE in "${FILES[@]}"; do
-    mpiexec -np 4 python data_conversion.py --input "$FILE" --output "$OUTPUT_PATH" --log "$LOG_PATH"
+    FILE_PATH="$DATA_PATH/$FILE"
+    mpiexec -np 4 python your_script.py --input "$FILE_PATH" --output "$OUTPUT_PATH" --log "$LOG_PATH"
 done
+
