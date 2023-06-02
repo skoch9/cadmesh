b0a43f8a9e8ec13702b45a113b1d7cd297342c02
Chandu <20564967+itsmeeChandU@users.noreply.github.com>
2023-06-02 10:50:24 -0700

    updated sample script

--- cadmesh/utils/sample_script.sh
+++ cadmesh/utils/sample_script.sh
@@ -6,25 +6,22 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --array=0-4%5
 
-module load python/3.9.0
+module load python/3.9
 module load mpi4py/3.0.3
 
-# Get the username for the path
-USER_NAME=$USER
 
-# Define paths relative to the scratch directory
-DATA_PATH="/scratch/$USER_NAME/Fusion360/segmentation/step/s2.0.1_extended_step/breps/step"
-OUTPUT_PATH="/scratch/$USER_NAME/Fusion360/segmentation/yaml/s2.0.1_extended"
-LOG_PATH="/scratch/$USER_NAME/Fusion360/segmentation/yaml/logs"
+DATA_PATH="$SCRATCH/madduri/Fusion360/segmentation/step/s2.0.1_extended_step/breps/step"
+OUTPUT_PATH="$SCRATCH/madduri/Fusion360/segmentation/yaml/s2.0.1_extended"
+LOG_PATH="$SCRATCH/madduri/Fusion360/segmentation/yaml/logs"
 
 # Get the batch of files this task should process
 FILES=($(ls -1 "$DATA_PATH" | sed -n "$((SLURM_ARRAY_TASK_ID*100+1)),$((SLURM_ARRAY_TASK_ID*100+100))p"))
 
 # Activate your Python environment
-source projects/def-teseo/$USER_NAME/envs/cad/bin/activate
+source ~/envs/cad/bin/activate
 
 for FILE in "${FILES[@]}"; do
     FILE_PATH="$DATA_PATH/$FILE"
-    mpiexec -np 4 python your_script.py --input "$FILE_PATH" --output "$OUTPUT_PATH" --log "$LOG_PATH"
+    mpiexec -np 4 python data_conversion.py --input "$FILE_PATH" --output "$OUTPUT_PATH" --log "$LOG_PATH"
 done
 
