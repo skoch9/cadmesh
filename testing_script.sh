8de557c1dc35525bd6cf5f1b0719ece4798be8b0
Chandu <20564967+itsmeeChandU@users.noreply.github.com>
2023-06-10 11:58:07 -0700

    new testing script for interactive job

--- testing_script.sh
+++ testing_script.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-#SBATCH --time=1:00:00
-#SBATCH --account=def-teseo
-#SBATCH --job-name=step-process
-#SBATCH --mem-per-cpu=16G
-#SBATCH --cpus-per-task=1
-#SBATCH --array=0-99
-
 
 # Activate Python environment
 source ~/scratch/madduri/cad/bin/activate
@@ -16,15 +9,15 @@ OUTPUT_PATH="/home/madduri/scratch/madduri/Fusion360/segmentation/yaml/s2.0.1_ex
 LOG_PATH="/home/madduri/scratch/madduri/Fusion360/segmentation/yaml/logs"
 HDF5_PATH="/home/madduri/scratch/madduri/Fusion360/segmentation/hdf5"
 
-BATCH_ID=$SLURM_ARRAY_TASK_ID  # BatchID is equal to the task id in the job array
-JOB_ID=$SLURM_JOB_ID           # JobID from SLURM
+BATCH_ID=0  # For interactive testing, we manually set BatchID
+JOB_ID="TestJob"  # Set a test JobID
 
 # Define new path for this batch
 BATCH_PATH="$DATA_PATH/batch_$BATCH_ID"
 mkdir -p "$BATCH_PATH"
 
 # Get the list of files to be copied
-FILES_TO_COPY=$(ls -1 "$DATA_PATH"/*.stp | sed -n "$((SLURM_ARRAY_TASK_ID*500+1)),$((SLURM_ARRAY_TASK_ID*500+500))p")
+FILES_TO_COPY=$(ls -1 "$DATA_PATH"/*.stp "$DATA_PATH"/*.step 2> /dev/null | sed -n "$((BATCH_ID*500+1)),$((BATCH_ID*500+500))p")
 
 # Check if files exist
 if [ -z "$FILES_TO_COPY" ]
@@ -36,9 +29,5 @@ fi
 # Copy the files
 cp $FILES_TO_COPY "$BATCH_PATH"
 
-
 # Conversion scripts
-python cloud_conversion.py --input "$DATA_PATH" --output "$OUTPUT_PATH" --log "$LOG_PATH" --batchId "$BATCH_ID" --jobId "$JOB_ID" --hdf5_file "$HDF5_PATH"
-
-## Delete processed files after the conversion
-#rm -r "$BATCH_PATH"
+python cloud_conversion.py --input "$BATCH_PATH" --output "$OUTPUT_PATH" --log "$LOG_PATH" --batchId "$BATCH_ID" --jobId "$JOB_ID" --hdf5_file "$HDF5_PATH"
