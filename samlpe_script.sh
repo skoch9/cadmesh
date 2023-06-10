1fed192972511f2b0730f99090d71f94ad2af0d3
Chandu <20564967+itsmeeChandU@users.noreply.github.com>
2023-06-10 12:48:02 -0700

    updated sample scripts with comments and fixed issues

--- samlpe_script.sh
+++ samlpe_script.sh
@@ -8,9 +8,12 @@
 
 
 # Activate Python environment
+echo "Activating Python environment..."
+module load python/3.8.10
 source ~/scratch/madduri/cad/bin/activate
 
 # Define paths relative to the scratch directory
+echo "Defining paths..."
 DATA_PATH="/home/madduri/scratch/madduri/Fusion360/segmentation/step/s2.0.1_extended_step/breps/step"
 OUTPUT_PATH="/home/madduri/scratch/madduri/Fusion360/segmentation/yaml/s2.0.1_extended"
 LOG_PATH="/home/madduri/scratch/madduri/Fusion360/segmentation/yaml/logs"
@@ -20,10 +23,12 @@ BATCH_ID=$SLURM_ARRAY_TASK_ID  # BatchID is equal to the task id in the job arra
 JOB_ID=$SLURM_JOB_ID           # JobID from SLURM
 
 # Define new path for this batch
+echo "Defining new path for this batch..."
 BATCH_PATH="$DATA_PATH/batch_$BATCH_ID"
 mkdir -p "$BATCH_PATH"
 
 # Get the list of files to be copied
+echo "Getting the list of files to be copied..."
 FILES_TO_COPY=$(ls -1 "$DATA_PATH"/*.stp | sed -n "$((SLURM_ARRAY_TASK_ID*500+1)),$((SLURM_ARRAY_TASK_ID*500+500))p")
 
 # Check if files exist
@@ -34,11 +39,12 @@ then
 fi
 
 # Copy the files
+echo "Copying the files..."
 cp $FILES_TO_COPY "$BATCH_PATH"
 
 
 # Conversion scripts
-python cloud_conversion.py --input "$DATA_PATH" --output "$OUTPUT_PATH" --log "$LOG_PATH" --batchId "$BATCH_ID" --jobId "$JOB_ID" --hdf5_file "$HDF5_PATH"
+echo "Running conversion scripts..."
+python cloud_conversion.py --input "$BATCH_PATH" --output "$OUTPUT_PATH" --log "$LOG_PATH" --batchId "$BATCH_ID" --jobId "$JOB_ID" --hdf5_file "$HDF5_PATH"
 
-## Delete processed files after the conversion
-#rm -r "$BATCH_PATH"
+echo "Done!"
--- testing_script.sh
+++ testing_script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Activate Python environment
+echo "Activating Python environment..."
 source ~/scratch/madduri/cad/bin/activate
 
 # Define paths relative to the scratch directory
+echo "Defining paths..."
 DATA_PATH="/home/madduri/scratch/madduri/Fusion360/segmentation/step/s2.0.1_extended_step/breps/step"
 OUTPUT_PATH="/home/madduri/scratch/madduri/Fusion360/segmentation/yaml/s2.0.1_extended"
 LOG_PATH="/home/madduri/scratch/madduri/Fusion360/segmentation/yaml/logs"
@@ -13,11 +15,13 @@ BATCH_ID=0  # For interactive testing, we manually set BatchID
 JOB_ID="TestJob"  # Set a test JobID
 
 # Define new path for this batch
+echo "Defining new path for this batch..."
 BATCH_PATH="$DATA_PATH/batch_$BATCH_ID"
 mkdir -p "$BATCH_PATH"
 
-# Get the list of files to be copied
-FILES_TO_COPY=$(ls -1 "$DATA_PATH"/*.stp "$DATA_PATH"/*.step 2> /dev/null | sed -n "$((BATCH_ID*500+1)),$((BATCH_ID*500+500))p")
+# Get the list of files to be copied (only 50 files)
+echo "Getting the list of files to be copied..."
+FILES_TO_COPY=$(ls -1 "$DATA_PATH"/*.stp "$DATA_PATH"/*.step 2> /dev/null | sed -n "$((BATCH_ID*50+1)),$((BATCH_ID*50+50))p")
 
 # Check if files exist
 if [ -z "$FILES_TO_COPY" ]
@@ -27,7 +31,12 @@ then
 fi
 
 # Copy the files
+echo "Copying the files..."
 cp $FILES_TO_COPY "$BATCH_PATH"
 
 # Conversion scripts
+echo "Running conversion scripts..."
 python cloud_conversion.py --input "$BATCH_PATH" --output "$OUTPUT_PATH" --log "$LOG_PATH" --batchId "$BATCH_ID" --jobId "$JOB_ID" --hdf5_file "$HDF5_PATH"
+
+echo "Done!"
+
